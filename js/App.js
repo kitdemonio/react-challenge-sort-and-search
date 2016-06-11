acff5fe284c58f93bf61d3a31272135bec79acbd
kitdemonio <ddd48533@gmail.com>
2016-06-11 18:31:49 +0200

    Add receiving data from server

--- js/App.js
+++ js/App.js
@@ -6,14 +6,38 @@ import Toolbar from './components/toolbar/toolbar';
 export default class App extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            data: []
+        };
+        this.getData()
+    }
+    
+
+    getData() {
+        $.ajax({
+            url: 'data.json',
+            dataType: 'json',
+            type: 'GET',
+            success: function(data) {
+                this.setState({data: data},function () {
+                    this.showStateData()
+                });
+            }.bind(this),
+            error: function(xhr, status, err) {
+                console.error('data.json', status, err.toString());
+            }.bind(this)
+        });
     }
 
+    showStateData() {
+        console.log(this.state.data);
+    }
 
     render() {
         return (
             <div className="container app">
                 <SearchBar />
-                <Toolbar />
+                <Toolbar data={this.state.data}/>
             </div>
         );
     }
--- js/components/toolbar/toolbar.js
+++ js/components/toolbar/toolbar.js
@@ -4,12 +4,20 @@ import UserData from './userdata/userdata';
 export default class ToolBar extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            data: this.props.data
+        };
     }
 
     render() {
         return (
             <div>
-                <UserData />
+                {this.props.data.map(function (value) { // Only for test
+                    return (
+                        <p key={value.id}>{value.name}</p>
+                    )
+                })}
+                <UserData data={this.state.data}/>
             </div>
         )
     }
